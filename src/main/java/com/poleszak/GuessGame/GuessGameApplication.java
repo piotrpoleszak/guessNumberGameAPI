3ec35fa2fec1f273d2fcbeb4f55f15bef17fba5f
Grindelwald <grindelwald@wingworm>
2022-04-01 17:12:42 +0200

    Initial commit

---
+++ src/main/java/com/poleszak/GuessGame/GuessGameApplication.java
@@ -0,0 +1,13 @@
+package com.poleszak.GuessGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poleszak/GuessGame/GuessGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poleszak.GuessGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
