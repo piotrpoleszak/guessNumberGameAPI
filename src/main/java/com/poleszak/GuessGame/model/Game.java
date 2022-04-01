ba968617da1d92c143dd41393a30ca1f61a14198
Grindelwald <grindelwald@wingworm>
2022-04-02 00:23:40 +0200

    Game and Guess classes

--- src/main/java/com/poleszak/GuessGame/model/Game.java
+++ src/main/java/com/poleszak/GuessGame/model/Game.java
@@ -1,4 +1,43 @@
 package com.poleszak.GuessGame.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "game_id")
+    private Long id;
+
+    @Column(name = "secret_number")
+    private int secretNumber;
+
+    @Column(name = "number_of_attempts")
+    private int numberOfAttempts;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+    
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "end_game_time")
+    private LocalDateTime endGameTime;
 }
--- src/main/java/com/poleszak/GuessGame/model/Guess.java
+++ src/main/java/com/poleszak/GuessGame/model/Guess.java
@@ -1,4 +1,12 @@
 package com.poleszak.GuessGame.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Guess {
+
+    private Long gameId;
+    private int guess;
 }
